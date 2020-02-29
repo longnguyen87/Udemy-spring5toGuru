5432745b74ce96fc230d8b61fc731bda5f5003c9
Long Nguyen <nguyenhlongvn@gmail.com>
2020-02-29 09:44:17 -0600

    Adding publisher

--- Chap1/src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ Chap1/src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -4,20 +4,25 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.example.demo.domain.Book;
+import com.example.demo.domain.Publisher;
 import com.example.demo.domain.Author;
 import com.example.demo.repository.AuthorRepository;
 import com.example.demo.repository.BookRepository;
+import com.example.demo.repository.PublisherRepository;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
 	
 	private final AuthorRepository authorRepo;
 	private final BookRepository bookRepo;
-	
-	public BootStrapData(AuthorRepository authorRepo, BookRepository bookRepo) {
+	private final PublisherRepository pubRepo;
+
+	public BootStrapData(AuthorRepository authorRepo, BookRepository bookRepo, PublisherRepository pubRepo) {
 		super();
 		this.authorRepo = authorRepo;
 		this.bookRepo = bookRepo;
+		this.pubRepo = pubRepo;
+
 	}
 
 	@Override
@@ -37,9 +42,21 @@ public class BootStrapData implements CommandLineRunner {
 
         authorRepo.save(rod);
         bookRepo.save(noEJB);
+        
+        // Create POJO Publisher
+        
+        Publisher pub1 = new Publisher();
 
+        pub1.setName("Long Nguyen");
+        pub1.setAddress("1968 Anton Way");
+        
+        pubRepo.save(pub1);
+        
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepo.count());
+        System.out.println("Publisher: " + pubRepo.count());
 	}
 
 }
+
+
---
+++ Chap1/src/main/java/com/example/demo/domain/Publisher.java
@@ -0,0 +1,91 @@
+package com.example.demo.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Publisher {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String name;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	
+	public Publisher(){
+		
+	}
+	
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Publisher other = (Publisher) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Publisher [id=" + id + ", name=" + name + ", address=" + address + ", city=" + city + ", state=" + state
+				+ ", zip=" + zip + "]";
+	}
+
+
+	
+	
+	
+}
